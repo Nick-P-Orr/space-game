3efb059c1fb07117db3221c836ca7b6026a42ec5
Nick <nick.p.orr@gmail.com>
2018-11-20 00:45:23 -0500

    Initial Commit

---
+++ SpaceGame.py
@@ -0,0 +1,12 @@
+
+#game like asteroids
+
+#Able to move in any direction from top down, and enemies will track the player
+#Confined to a certain sized, square stage
+
+#Classes of ships, chain lightning, cone/slow magnetic, typical laser, and a heatseeking missle which tracks enemies
+
+#Different enemies. Light kamikaze unit which attempts to crash into the player ship, heavy kamikaze unit but slowwer, and a ranged enemy
+#Different effects, which are applied to enemies as the game gets harder. Units will start corroding the ship if you are hit, which does damage over time
+
+#Bosses, which will have high health and abilities similar to the user playable ships. Light units will still spawn during this time.
